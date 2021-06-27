3e94af4b2cd1d0740cfeb1358d3d426a6727e35d
Varugheson-pVarughese <86556523+Varugheson-pVarughese@users.noreply.github.com>
2021-06-27 15:02:47 +0100

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Basic Web Development React.js Bootcamp </p>
+    </div>
+  );
+}
+export default Info;
